1f421dee8b14a336bc32d218e666fe5d3c4cc81a
PantherianCodeX <PantherianCodeX@pm.me>
2025-09-19 23:47:14 -0600

    Dev static: serve /static via ASGIStaticFilesHandler under Daphne when DEBUG=1 to fix admin CSS/JS 404s

--- apps/platform/config/asgi.py
+++ apps/platform/config/asgi.py
@@ -1,5 +1,6 @@
 import os
 from django.core.asgi import get_asgi_application
+from django.conf import settings
 
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE", os.getenv("DJANGO_SETTINGS_MODULE", "apps.platform.config.settings.dev")
@@ -11,12 +12,19 @@ try:
     from channels.routing import ProtocolTypeRouter, URLRouter
     from channels.auth import AuthMiddlewareStack
     from apps.platform.config.routing import websocket_urlpatterns
+    # Serve static files in development (DEBUG) so /static/admin/* works under Daphne
+    if getattr(settings, "DEBUG", False):
+        try:
+            from django.contrib.staticfiles.handlers import ASGIStaticFilesHandler
+            http_app = ASGIStaticFilesHandler(django_asgi_app)
+        except Exception:
+            http_app = django_asgi_app
+    else:
+        http_app = django_asgi_app
 
-    application = ProtocolTypeRouter(
-        {
-            "http": django_asgi_app,
-            "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
-        }
-    )
+    application = ProtocolTypeRouter({
+        "http": http_app,
+        "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+    })
 except Exception:
     application = django_asgi_app
